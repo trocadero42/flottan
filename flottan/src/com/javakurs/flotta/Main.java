8acb2625dfb0c30dc3e7be1c84ca7ad9cca11c2f
Gunnar Lindström <gunnar@lmdv.se>
2020-01-20 13:53:48 +0100

    second commit

---
+++ flottan/src/com/javakurs/flotta/Main.java
@@ -0,0 +1,8 @@
+package com.javakurs.flotta;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ flottan/src/com/javakurs/flotta/SQL.java
@@ -0,0 +1,38 @@
+package com.javakurs.flotta;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SQL {
+
+    // private static final String useSSLFalse  = "?useSSL=false";
+    private static final String useSSLTrue   = "?useSSL=true";
+    private static final String DATABASE     = "ships";
+    private static final String URL          = "jdbc:mysql://flottan.mysql.database.azure.com:3306/" + DATABASE + useSSLTrue;
+    private static final String USERNAME     = "goow@flottan";
+    private static final String PASSWORD     = "Nackademin!123";
+
+
+    public static void getDatabaseList () {
+
+        // String sql_query = "select * from " + table + ";";
+        String sql_query = "show databases;";
+
+        try {
+
+            Connection myConn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement myStatement = myConn.createStatement();
+
+            ResultSet myResult = myStatement.executeQuery(sql_query);
+
+            while (myResult.next()) {
+                System.out.println(myResult.getString("Database"));
+            }
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
